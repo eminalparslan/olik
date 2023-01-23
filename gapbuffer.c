acb3013e9cb9646413076ba11122dee2fbb2ba46
eminalparslan <eminalparslan@gmail.com>
2023-01-22 23:07:17 -0600

    comments

--- gapbuffer.c
+++ gapbuffer.c
@@ -1,8 +1,11 @@
 #include "gapbuffer.h"
 
+// Initial capacity of a string
 #define STRING_INIT_CAP 16
 
+/* Append a character to a string s. */
 void strAppendChar(String *s, char c) {
+  // Check if capacity needs to grow
   if (s->size >= s->capacity) {
     s->capacity = s->capacity == 0 ? STRING_INIT_CAP : s->capacity * 2;
     s->chars = (char *) realloc(s->chars, s->capacity * sizeof(*(s->chars)));
@@ -11,39 +14,51 @@ void strAppendChar(String *s, char c) {
   s->chars[s->size++] = c;
 }
 
+/* Append many characters to a string s. */
 void strAppendChars(String *s, const char *cs, int length) {
+  // Check if capacity needs to grow
   if (s->size + length > s->capacity) {
     s->capacity = s->capacity == 0 ? STRING_INIT_CAP + length : (s->size + length) * 2;
     s->chars = (char *) realloc(s->chars, s->capacity * sizeof(*(s->chars)));
     assert(s->chars != NULL);
   }
-  memcpy(s->chars + s->size, cs, length * sizeof(*(s->chars)));
+  // Copy chars to strings
+  memcpy(&s->chars[s->size], cs, length * sizeof(*(s->chars)));
   s->size += length;
 }
 
+/* Prepend characters to a string s. */
 void strPrependChars(String *s, const char *cs, int length) {
-  if (s->size + length >= s->capacity) {
+  // Check if capacity needs to grow
+  if (s->size + length > s->capacity) {
     s->capacity = (s->size + length) * 2;
     s->chars = (char *) realloc(s->chars, s->capacity * sizeof(*(s->chars)));
     assert(s->chars != NULL);
   }
-  memmove(s->chars + length, s->chars, s->size * sizeof(*(s->chars)));
+  // Shift string chars to make room at the beginning
+  memmove(&s->chars[length], s->chars, s->size * sizeof(*(s->chars)));
+  // Copy chars to beginning
   memcpy(s->chars, cs, length * sizeof(*(s->chars)));
   s->size += length;
 }
 
+/* Empty the string. */
 void strClear(String *s) {
   s->size = 0;
 }
 
+/* Return the length of the gap buffer. */
 size_t gbLen(GapBuffer *buf) {
   return buf->head.size + buf->tail.size;
 }
 
+/* Write nbyte bytes of gap buffer buf to fildes.
+   Returns the total bytes written. */
 size_t gbWrite(int fildes, GapBuffer *buf, size_t nbyte) {
   assert(nbyte <= gbLen(buf));
   int written = 0;
   if (nbyte < buf->head.size) {
+    // Only need to write chars from head string
     written += write(fildes, buf->head.chars, nbyte);
   } else {
     written += write(fildes, buf->head.chars, buf->head.size);
@@ -52,43 +67,52 @@ size_t gbWrite(int fildes, GapBuffer *buf, size_t nbyte) {
   return written;
 }
 
+/* Move the gap in the gap buffer to pos. */
 void gbMoveGap(GapBuffer *buf, int pos) {
-  // FIXME: pos is at len of buf
   assert(pos >= 0 && pos <= gbLen(buf));
 
   int headLength = buf->head.size;
   if (pos < headLength) {
-    strPrependChars(&buf->tail, buf->head.chars + pos, buf->head.size - pos);
+    // Take the chars in head string after pos and prepend them to tail string
+    strPrependChars(&buf->tail, &buf->head.chars[pos], buf->head.size - pos);
     buf->head.size = pos;
   } else if (pos > headLength) {
     pos -= headLength;
+    // Append the first pos chars in tail string to head string
     strAppendChars(&buf->head, buf->tail.chars, pos);
     buf->tail.size -= pos;
-    memmove(buf->tail.chars, buf->tail.chars + pos, buf->tail.size * sizeof(*(buf->tail.chars)));
+    // Shift the tail string chars after pos to the beginning
+    memmove(buf->tail.chars, &buf->tail.chars[pos], buf->tail.size * sizeof(*(buf->tail.chars)));
   }
 }
 
+/* Inserts a character at the gap. */
 void gbInsertChar(GapBuffer *buf, char c) {
   strAppendChar(&buf->head, c);
 }
 
+/* Inserts length characters at the gap. */
 void gbInsertChars(GapBuffer *buf, const char *cs, int length) {
   strAppendChars(&buf->head, cs, length);
 }
 
+/* Deletes the character before the gap. */
 char gbDeleteChar(GapBuffer *buf) {
   assert(buf->head.size > 0);
   return buf->head.chars[--buf->head.size];
 }
 
+/* Pushes a character to the end of the gap buffer. */
 void gbPushChar(GapBuffer *buf, char c) {
   strAppendChar(&buf->tail, c);
 }
 
+/* Pushes length characters to the end of the gap buffer. */
 void gbPushChars(GapBuffer *buf, const char *cs, int length) {
   strAppendChars(&buf->tail, cs, length);
 }
 
+/* Pops a character off the end of the gap buffer. */
 char gbPopChar(GapBuffer *buf) {
   assert(gbLen(buf) > 0);
   if (buf->tail.size > 0) {
@@ -98,26 +122,33 @@ char gbPopChar(GapBuffer *buf) {
   }
 }
 
+/* Appends the characters of src to dst. */
 void gbConcat(GapBuffer *dst, GapBuffer *src) {
   gbPushChars(dst, src->head.chars, src->head.size);
   gbPushChars(dst, src->tail.chars, src->tail.size);
 }
 
+/* Splits src at gap and pushes tail from src to dst. */
 void gbSplit(GapBuffer *dst, GapBuffer *src) {
   gbPushChars(dst, src->tail.chars, src->tail.size);
   strClear(&src->tail);
 }
 
+/* Prints the gap buffer at fildes. */
 void gbPrint(GapBuffer *buf, int fildes) {
   gbWrite(fildes, buf, gbLen(buf));
   const char nl = '\n';
   write(fildes, &nl, 1);
 }
 
+/* Creates a new gap buffer. */
 GapBuffer *gbCreate() {
-  return calloc(1, sizeof(GapBuffer));
+  GapBuffer *gbNew = calloc(1, sizeof(GapBuffer));
+  assert(gbNew != NULL);
+  return gbNew;
 }
 
+/* Frees the gap buffer. */
 void gbFree(GapBuffer *buf) {
   free(buf);
 }
--- olik.c
+++ olik.c
@@ -10,25 +10,27 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+// Starting capacity of lines in editor
 #define EDITOR_LINES_CAP 8
-#define DEBUG 1
-
-enum EditorMode { Normal, Insert };
 
 /*
   TODO:
-   - working prototype
+   - opening and saving files
    - free lines after closing file
    - skip list for lines
    - error checking
 */
 
+enum EditorMode { Normal, Insert };
+
+// Stores the lines of the files as an arary of gap buffers
 typedef struct {
   GapBuffer **bufs;
   size_t size;
   size_t capacity;
 } Lines;
 
+// Editor state and contents
 typedef struct {
   Lines lines;
   int width, height;
@@ -39,6 +41,7 @@ typedef struct {
 
 struct termios orig_termios;
 
+// ANSI escape wrapper functions: https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 void eraseScreen() { printf("\x1b[2J"); }
 void eraseRestScreen() { printf("\x1B[0J"); }
 void eraseLine() { printf("\x1B[2K\r"); }
@@ -48,6 +51,7 @@ void moveCursorRight(int n) { printf("\x1B[%dC", n); }
 void setCursorPos(int row, int col) { printf("\x1B[%d;%df", row+1, col+1); }
 void setCursorCol(int col) { printf("\x1B[%dG", col+1); }
 
+/* Prints the editor content to stderr. */
 void debugEditor(Editor *e) {
   fprintf(stderr, "struct Editor {\n");
   fprintf(stderr, "  struct Lines {\n");
@@ -62,25 +66,32 @@ void debugEditor(Editor *e) {
   fprintf(stderr, "}\n");
 }
 
+/* Clears the screen and moves the cursor home. */
 void clearScreen() {
   eraseScreen();
   moveCursorHome();
 }
 
+/* Helper function for error checking. */
 void die(const char *s) {
   clearScreen();
   perror(s);
   exit(1);
 }
 
+/* Disables raw mode. Called at program exit. */
 void disableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+  clearScreen();
 }
 
+/* Enables raw mode and saves original termios state. */
 void enableRawMode() {
   if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  // Restore terminal to original state at program exit
   atexit(disableRawMode);
 
+  // Raw mode settings
   struct termios raw_termios = orig_termios;
   raw_termios.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
   raw_termios.c_oflag &= ~(OPOST);
@@ -90,9 +101,11 @@ void enableRawMode() {
   raw_termios.c_cc[VTIME] = 1;
 
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_termios) == -1) die("tcsetattr");
+  // turn off stdout buffer
   setbuf(stdout, NULL);
 }
 
+/* Gets the window size of the terminal in rows and cols. */
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
@@ -105,7 +118,9 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/* Append a gap buffer to the lines. */
 void linesAppend(Lines *l, GapBuffer *buf) {
+  // Grow lines capacity if needed
   if (l->size >= l->capacity) {
     l->capacity = l->capacity == 0 ? EDITOR_LINES_CAP : l->capacity * 2;
     l->bufs = (GapBuffer **) realloc(l->bufs, l->capacity * sizeof(*(l->bufs)));
@@ -114,34 +129,42 @@ void linesAppend(Lines *l, GapBuffer *buf) {
   l->bufs[l->size++] = buf;
 }
 
+/* Insert a gap buffer in the lines at pos. */
 void linesInsert(Lines *l, GapBuffer *buf, int pos) {
+  // Grow lines capacity if needed
   if (l->size >= l->capacity) {
     l->capacity = l->capacity == 0 ? EDITOR_LINES_CAP : l->capacity * 2;
     l->bufs = (GapBuffer **) realloc(l->bufs, l->capacity * sizeof(*(l->bufs)));
     assert(l->bufs != NULL);
   }
+  // Shifts gap buffer pointers after pos by 1
   memmove(&l->bufs[pos+1], &l->bufs[pos], (l->size - pos) * sizeof(*(l->bufs)));
   l->bufs[pos] = buf;
   l->size++;
 }
 
+/* Deletes and frees the gap buffer at pos in lines. */
 void linesDelete(Lines *l, int pos) {
   gbFree(l->bufs[pos]);
+  // Shifts gap buffer pointers after pos back by 1
   memmove(&l->bufs[pos], &l->bufs[pos+1], (l->size - pos - 1) * sizeof(*(l->bufs)));
   l->size--;
 }
 
+/* Returns the gap buffer at the given row. */
 GapBuffer *getLine(Editor *e, int row) {
   if (row < e->lines.size)
     return e->lines.bufs[row];
   return NULL;
 }
 
+/* Initializes the editor state. The editor should be allocated with calloc. */
 void initEditor(Editor *e) {
   linesAppend(&e->lines, gbCreate());
   if (getWindowSize(&e->height, &e->width) == -1) die("getWindowSize");
 }
 
+/* Helper min function. */
 int min(int a, int b) {
   if (a < b) {
     return a;
@@ -149,16 +172,21 @@ int min(int a, int b) {
   return b;
 }
 
+/* Moves the cursor left by n. */
 void cursorLeft(Editor *e, int n) {
+  if (n <= 0 ) return;
   if (e->col - n >= 0) {
     moveCursorLeft(n);
     e->col -= n;
   }
 }
 
+/* Moves the cursor down by n. Scrolls if needed. */
 void cursorDown(Editor *e, int n) {
+  if (n <= 0 ) return;
   if (e->row - e->offset + n < min(e->height, e->lines.size - e->offset)) {
     e->row += n;
+    // Stay on the text
     int len = gbLen(getLine(e, e->row));
     if (e->col > len) {
       e->col = len;
@@ -169,9 +197,12 @@ void cursorDown(Editor *e, int n) {
   }
 }
 
+/* Moves the cursor up by n. Scrolls if needed. */
 void cursorUp(Editor *e, int n) {
+  if (n <= 0 ) return;
   if (e->row - e->offset - n >= 0) {
     e->row -= n;
+    // Stay on the text
     int len = gbLen(getLine(e, e->row));
     if (e->col > len) {
       e->col = len;
@@ -182,13 +213,16 @@ void cursorUp(Editor *e, int n) {
   }
 }
 
+/* Moves the cursor right by n. */
 void cursorRight(Editor *e, int n) {
+  if (n <= 0 ) return;
   if (e->col < gbLen(getLine(e, e->row))) {
     moveCursorRight(n);
     e->col += n;
   }
 }
 
+/* Renders the current line. */
 void renderLine(Editor *e) {
   eraseLine();
   GapBuffer *gb = getLine(e, e->row);
@@ -196,6 +230,7 @@ void renderLine(Editor *e) {
   setCursorCol(e->col);
 }
 
+/* Render all the lines after startRow. */
 void renderLinesAfter(Editor *e, int startRow) {
   setCursorPos(startRow, 0);
   eraseRestScreen();
@@ -207,20 +242,30 @@ void renderLinesAfter(Editor *e, int startRow) {
   setCursorPos(e->row, e->col);
 }
 
+/* Handle backspace. */
 void backspace(Editor *e) {
   GapBuffer *gbCur = getLine(e, e->row);
   if (e->col == 0) {
     if (e->row == 0) return;
+    // Backspace at start of line
     GapBuffer *gbPrev = getLine(e, e->row - 1);
+    size_t prevLen = gbLen(gbPrev);
+    // Append current line to the end of previous line
     gbConcat(gbPrev, gbCur);
+    // Delete current line
     linesDelete(&e->lines, e->row);
+    // Move cursor up and to the end of original text
     cursorUp(e, 1);
+    cursorRight(e, prevLen);
+    // Render new lines
     renderLinesAfter(e, e->row);
   } else if (e->col == gbLen(gbCur)) {
+    // Backspace at end of line
     gbPopChar(gbCur);
     e->col--;
     renderLine(e);
   } else if (e->col < gbLen(gbCur)) {
+    // Backspace in the middle of the line
     gbMoveGap(gbCur, e->col);
     gbDeleteChar(gbCur);
     e->col--;
@@ -228,16 +273,20 @@ void backspace(Editor *e) {
   }
 }
 
+/* Handle new line (enter). */
 void newLine(Editor *e) {
   GapBuffer *gbCur = getLine(e, e->row);
   gbMoveGap(gbCur, e->col);
   GapBuffer *gbNew = gbCreate();
+  // Split the current line at col, and put the second half in gbNew
   gbSplit(gbNew, gbCur);
   renderLine(e);
 
   if (e->row == e->lines.size) {
+    // If its the last line, just append
     linesAppend(&e->lines, gbNew);
   } else {
+    // Otherwise, insert the new gap buffer
     linesInsert(&e->lines, gbNew, e->row + 1);
   }
   cursorDown(e, 1);
@@ -245,21 +294,27 @@ void newLine(Editor *e) {
   renderLinesAfter(e, e->row);
 }
 
+/* Load file into editor buffer. */
 void loadFile(Editor *e) {
 
 }
 
+/* Save editor buffer into file. */
 void saveFile(Editor *e) {
 
 }
 
+/* Write a character to the terminal screen. */
 void writeCh(Editor *e, char ch) {
   GapBuffer *gb = getLine(e, e->row);
   int lineLength = gbLen(gb);
   assert(e->col <= lineLength);
+
   if (e->col == lineLength) {
+    // If at the end, just append
     gbPushChar(gb, ch);
   } else {
+    // Otherwise, insert
     gbMoveGap(gb, e->col);
     gbInsertChar(gb, ch);
   }
@@ -267,6 +322,7 @@ void writeCh(Editor *e, char ch) {
   renderLine(e);
 }
 
+/* Get the next character input. */
 char getCh() {
   int nread;
   char c;
@@ -276,9 +332,11 @@ char getCh() {
   return c;
 }
 
+/* Handle the next character input. */
 bool processChar(Editor *e) {
   char c = getCh();
   if (e->mode == Normal) {
+    // Deal with normal node keys.
     switch (c) {
       case 'q':
         return true;
@@ -301,6 +359,7 @@ bool processChar(Editor *e) {
         break;
     }
   } else if (e->mode == Insert) {
+    // Deal with insert mode keys.
     if (isprint(c)) {
       writeCh(e, c);
     } else {
@@ -320,7 +379,7 @@ bool processChar(Editor *e) {
           }
           break;
         default:
-          printf("%d", c);
+          //printf("%d", c);
           break;
       }
     }
@@ -334,18 +393,14 @@ int main() {
 
   Editor *e;
   e = (Editor *) calloc(1, sizeof(Editor));
-  if (e == NULL) {
-    fprintf(stderr, "Allocation failed\n");
-    exit(1);
-  }
+  if (e == NULL) die("Allocation failed\n");
   initEditor(e);
 
   bool quit = false;
   while (!quit) {
     quit = processChar(e);
-    debugEditor(e);
+    //debugEditor(e);
   };
 
-  clearScreen();
   return 0;
 }
